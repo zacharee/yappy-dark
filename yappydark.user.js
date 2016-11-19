8df5bd7771dedfbe19bede4c117be2c349af21a8
zacharee <zachary.wander@gmail.com>
2016-11-19 18:45:42 -0500

    Create yappydark.user.js
     - first creation

---
+++ yappydark.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         YAPPYDARK
+// @namespace    zachary@techyteen.tk
+// @version      0.1a
+// @description  Dark theme for Yappy Web
+// @author       Zachary Wander
+
+// @include      *://*yappy.im/web/*
+
+// @grant        none
+// ==/UserScript==
+
+/*global jQuery*/
+
+jQuery('head').append('<link rel="stylesheet" type="text/css" href="">');
